5c1931704f3c7270e209f51cdfea2e90b94aa560
wayared <jarecast@espol.edu.ec>
2023-03-24 22:40:03 -0700

    What are command line arguments (argc and argv)?

--- optarg1.c
+++ optarg1.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include<stdlib.h>
 
-int main(){
+int main(int argc, char* argv[]){
 	int i;
 	for(i = 1; i < argc; i++){
 	    printf("argv[%d]: %s\n", i, argv[i]);
@@ -11,7 +11,9 @@ int main(){
 	
 	int result = 1;
 	for(i = 1; i < argc; i++){
-	   int x =strol(argv[i],NULL,10);
+	   int x =strol(argv[i],NULL,10); //en el primer parametro es lo q tratamos de convertir , 
+					  //en el segundo parametro, 
+					  //en el tercero va la base de nuestro numero
 	   result = result * x;
 	}
 	printf("Multiplication result is %d\n", result);
