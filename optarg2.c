626752c74dbec50028774d20e187a8e1575ab521
wayared <jarecast@espol.edu.ec>
2023-03-28 23:44:15 -0700

    Using getopt

--- optarg2.c
+++ optarg2.c
@@ -4,6 +4,23 @@
 #include <stdio.h>
 #include <getopt.h>
 
+void print_usage(){
+	printf("Usage: temp -c <temp> | temp -f <temp> \n");
+	exit(2);	
+}
+
+void print_farenheit(){
+	float f = ((temp * 9) / 5) + 32;
+	printf("%.2f centigrade is %.2f fareheit\n",temp,f);
+}
+
+void print_centigrade(float temp){
+	float c = ((temp - 32) * 5) / 9;
+	printf("%.2f farenheit is %.2f centigrade\n",temp,c);
+
+}
+
+
 int main(int argc, char **argv){
 	int option;
 	while ((option = getopt(argc, argv, "cf")) != -1){
